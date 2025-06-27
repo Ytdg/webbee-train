1aafdbeaeffe1e77f68a10b1b9ac20ca45f95490
Nikita Bochkov <nikiljgryt@gmail.com>
2025-06-27 21:55:32 +0300

    feat: removed unnecessary functionality

--- java-patterns/src/main/java/org/example/proxy/CodeCollectorProxy.java
+++ java-patterns/src/main/java/org/example/proxy/CodeCollectorProxy.java
@@ -1,9 +1,6 @@
 package org.example.proxy;
 
-import java.util.HashMap;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
@@ -11,46 +8,36 @@ import java.util.stream.Stream;
 public class CodeCollectorProxy implements DataCollector<Channel<NumberSecret>> {
 
     private final CodeCollector codeCollector = new CodeCollector();
-    private HashMap<UUID, String> cacheCode = new HashMap<>(); //UUID implementation equals
+    private HashSet<UUID> cacheCode = new HashSet<>(); //UUID implementation equals
     private static final Logger LOGGER = Logger.getLogger(CodeCollectorProxy.class.getName());
 
     @Override
-    public boolean acceptData(Channel<NumberSecret> data, Consumer<String> transferResulData) {
+    public boolean acceptData(Channel<NumberSecret> data, Consumer<String> extractData) {
         Objects.requireNonNull(data);
-        Objects.requireNonNull(transferResulData);
-        boolean isSuccessfully = true;
-        for (NumberSecret num : data.getPartialData()) {
-            if (!cacheCode.containsKey(num.getUuid())) {
-                if (!codeCollector.acceptData(data, s -> cacheCode.put(num.getUuid(), s))) {
-                    isSuccessfully = false;
-                    LOGGER.info("Proxy error for id:" + num.getUuid().toString());
-                }
-            }
-            if (cacheCode.containsKey(num.getUuid())) {
-                transferResulData.accept(num.getEncryptedNumber());
-                LOGGER.info("Proxy for id:" + num.getUuid().toString());
-            }
+        Objects.requireNonNull(extractData);
+        boolean isSuccessfully;
+
+        if (Objects.isNull(cacheCode)) {
+            cacheCode = new HashSet<>();
         }
+        isSuccessfully = writeToCache(data, extractData);
+
         return isSuccessfully;
     }
 
-
-    @Override
-    public Stream<String> getStreamResult() {
-        if (Objects.nonNull(cacheCode)) {
-            return cacheCode.values().stream();
+    private boolean writeToCache(Channel<NumberSecret> data, Consumer<String> extractData) {
+        if (!cacheCode.contains(data.uuid())) {
+             cacheCode.add(data.uuid());
+            if (!codeCollector.acceptData(data, extractData)) {
+                LOGGER.info("Proxy error extract for id channel:" + data.uuid());
+                return false;
+            }
+        } else {
+            // extract data from other resource (redis as example)
         }
-        return codeCollector.getStreamResult();
+        return true;
     }
 
-
-    @Override
-    public Optional<String> getDataById(String id) {
-        if (Objects.nonNull(cacheCode)) {
-            return cacheCode.get(UUID.fromString(id)).describeConstable();
-        }
-        return codeCollector.getDataById(id);
-    }
 
     public void clearCache() {
         cacheCode = null; // method can be clear cache with schedule as example
