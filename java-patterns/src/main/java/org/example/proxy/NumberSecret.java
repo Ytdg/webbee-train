0e6cc4542a190f25357227b30c0f6b82b563e5c1
Nikita Bochkov <nikiljgryt@gmail.com>
2025-06-27 21:56:43 +0300

    feat: record class

--- java-patterns/src/main/java/org/example/proxy/NumberSecret.java
+++ java-patterns/src/main/java/org/example/proxy/NumberSecret.java
@@ -1,29 +1,11 @@
 package org.example.proxy;
 
 import java.util.Objects;
-import java.util.UUID;
 
-public class NumberSecret {
-
-    private final String encryptedNumber;
-    private final UUID uuid;
+public record NumberSecret(String encryptedNumber) {
 
     public NumberSecret(String encryptedNumber) {
-        this.uuid = UUID.randomUUID();
         this.encryptedNumber = Objects.requireNonNull(encryptedNumber);
     }
 
-    public String getEncryptedNumber() {
-        return this.encryptedNumber;
-    }
-
-    public UUID getUuid() {
-        return this.uuid;
-    }
-
-
-    @Override
-    public String toString() {
-        return "id:" + uuid.toString() + "|" + "encrypt:" + encryptedNumber;
-    }
 }
