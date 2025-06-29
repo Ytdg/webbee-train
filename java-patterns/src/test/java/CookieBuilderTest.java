51f83622cdc1c58564354ed5eb41ce20035393e9
Nikita Bochkov <nikiljgryt@gmail.com>
2025-06-29 19:52:10 +0300

    test (fix): change logging

--- java-patterns/src/test/java/CookieBuilderTest.java
+++ java-patterns/src/test/java/CookieBuilderTest.java
@@ -1,12 +1,15 @@
 import org.example.builder.Cookie;
 import org.junit.jupiter.api.Test;
 
+import java.util.logging.Logger;
+
 /**
  * Тест демонстрирует создание Cookie.
  * @author  Nikita Bochkov
  * */
 public class CookieBuilderTest {
 
+    private  static  final Logger logger=Logger.getLogger(CookieBuilderTest.class.getName());
     @Test
     void createInstance_withSetProperty() {
         Cookie cookie = new Cookie.Builder(false).domain("test.org")
@@ -14,13 +17,14 @@ public class CookieBuilderTest {
                 .language("en")
                 .idSession(10).build();
 
-        System.out.println(cookie+"\n");
+        logger.info(cookie+"\n");
 
         cookie =  new Cookie.Builder(false).domain("test.org")
                 .activePage(null)
                 .language(null)
                 .idSession(10).build();
-        System.out.println(cookie);
+
+        logger.info(cookie.toString());
     }
 
 }
