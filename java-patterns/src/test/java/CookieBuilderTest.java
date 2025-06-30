8077a1b6328f921eed1b1a6aded8c3d02699baf4
Nikita Bochkov <nikiljgryt@gmail.com>
2025-06-30 09:43:36 +0300

    test (fix):logger replaced with the assertions equals

--- java-patterns/src/test/java/CookieBuilderTest.java
+++ java-patterns/src/test/java/CookieBuilderTest.java
@@ -1,4 +1,5 @@
 import org.example.builder.Cookie;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.logging.Logger;
@@ -9,7 +10,7 @@ import java.util.logging.Logger;
  * */
 public class CookieBuilderTest {
 
-    private  static  final Logger logger=Logger.getLogger(CookieBuilderTest.class.getName());
+
     @Test
     void createInstance_withSetProperty() {
         Cookie cookie = new Cookie.Builder(false).domain("test.org")
@@ -17,14 +18,28 @@ public class CookieBuilderTest {
                 .language("en")
                 .idSession(10).build();
 
-        logger.info(cookie+"\n");
+        Assertions.assertEquals(
+                """
+                        Authenticated:false
+                        Active Page:num 1
+                        Id session:10
+                        Language:en
+                        Domain:test.org""",cookie.toString()
+        );
 
         cookie =  new Cookie.Builder(false).domain("test.org")
                 .activePage(null)
                 .language(null)
                 .idSession(10).build();
 
-        logger.info(cookie.toString());
+        Assertions.assertEquals(
+                """
+                        Authenticated:false
+                        Active Page:null
+                        Id session:10
+                        Language:null
+                        Domain:test.org""",cookie.toString()
+        );
     }
 
 }
